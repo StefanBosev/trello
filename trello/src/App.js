0442776090596e363900b8b741b6e5330f348798
Stefan Bosev <stefan.bosev1@gmail.com>
2022-04-18 20:08:54 +0300

    Added React project and started to make react columns

---
+++ trello/src/App.js
@@ -0,0 +1,34 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="trello">
+      <nav className="app-navbar">
+        Navbar
+      </nav>
+      <div className="board">
+        <div className="column">
+          <header>title</header>
+          <ul>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+          </ul>
+          <footer>add item</footer>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
